f2eb8f1dfe837ccbb3cd8e938269d511a73a333b
rewolf <andrew.flower@gmail.com>
2019-08-11 14:13:50 +0900

    Add display in React

--- src/main/webapp/javascript/Main.jsx
+++ src/main/webapp/javascript/Main.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
+import FriendList from './FriendList';
 import '../css/Main.css';
 
 class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            friends: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("/api/friends")
+            .then(res => res.json())
+            .then(
+                (response) => {
+                    this.setState({
+                        friends: response
+                    });
+                },
+                (error) => {
+                    alert(error);
+                }
+            )
+    }
+
     render() {
         return (
             <div id="main">
-                <h1>Demo Component</h1>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+                <h1>My Best Friends</h1>
+                <FriendList friends={this.state.friends}/>
             </div>
         );
     }
